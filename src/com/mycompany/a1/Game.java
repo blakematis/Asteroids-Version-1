04d0ff12c4555de7979bd621e362043321d82a91
Blake Matis <Blake@Macintosh-3.local>
2018-02-11 16:16:27 -0800

    JavaDoc Description and Constructor Definition
    Added javaDoc for the Game Class.
    Created a working constructor that encapsulates GameWorld, calls Init
    method of gameWorld then Calls private Play method of Game class.

--- src/com/mycompany/a1/Game.java
+++ src/com/mycompany/a1/Game.java
@@ -1,19 +1,48 @@
 package com.mycompany.a1;
+
+import com.codename1.ui.Form;
+
 /**
- * Model that encapsulates the game. Contains several components: GameWorld,
- * and play() method to accept and execute user commands.
+ * Primary class Game is a controller that encapsulates the flow of control in the game.
+ * Contains several components: GameWorld, and play() method to accept and execute user 
+ * commands. The Game controller enforces rules such as what actions a player may take
+ * and what happens as a result. This class accepts user input in the form of keyboard
+ * commands from the human player and invokes appropriate methods in the game world to
+ * perform the requested commands to manipulate data in the GameWorld model.
  * 
  * @author Blake Matis
  *
  */
-public class Game {
+public class Game extends Form{
 	
+	/*---------------------------------------DATA---------------------------------------*/
+	private GameWorld gw;
+
+	
+
+	/*-----------------------------------CONSTRUCTORS-----------------------------------*/
+	
+	/**
+	 * Default Constructor performs the following:
+	 * Game has-a GameWorld object. 
+	 * Game initializes the GameWorld object with the init() method.
+	 * Game then calls the play() method accepting user commands.
+	 */
+	public Game(){
+		this.gw = new GameWorld();
+		this.gw.init();
+		play();
+	}
+	
+
+	
+	/*----------------------------------PRIVATE METHODS---------------------------------*/
 	
 	/**
 	 * Accepts and executes user commands
 	 */
 	public void play(){
 		//TODO
+		
 	}
-
 }
--- src/com/mycompany/a1/GameWorld.java
+++ src/com/mycompany/a1/GameWorld.java
@@ -1,5 +1,32 @@
 package com.mycompany.a1;
-
+/**
+ * The GameWorld is a model that holds the program's data such as
+ * a collection of game GameObjects and other state variables.
+ * 
+ * @author Blake Matis
+ *
+ */
 public class GameWorld {
+	
+	/*---------------------------------------DATA---------------------------------------*/
+	
+	/*-----------------------------------CONSTRUCTORS-----------------------------------*/
+	
+	/*----------------------------------PRIVATE METHODS---------------------------------*/
+	
+	/*--------------------------------GETTERS AND SETTERS-------------------------------*/
 
+	/**
+	 * Default Constructor
+	 */
+	public GameWorld(){
+		//TODO default constructor
+	}
+	
+	/**
+	 * 
+	 */
+	public void init(){
+		
+	}
 }
