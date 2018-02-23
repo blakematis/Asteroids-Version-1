d44708e544761869d6215be5bdcea4e94a799e54
Blake Matis <Blake@Macintosh-3.local>
2018-02-22 19:40:31 -0800

    Added UniqueId per instance of FixedObjects

--- src/Objects/FixedObject.java
+++ src/Objects/FixedObject.java
@@ -5,19 +5,33 @@ package Objects;
  *
  */
 public abstract class FixedObject extends GameObject {
-
+	
+	private static int uniqueId = 1;
+	private int id;
+	
 	/**
 	 * 
 	 */
 	public FixedObject(){
 		super();
+		this.setId(uniqueId);
+		uniqueId++;
+		
 	}
 	
 	@Override
 	public String toString(){
 		String parentStr = super.toString();
-		String myString = parentStr;
+		String myString = parentStr + " , " +  id;
 		return myString;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 }
